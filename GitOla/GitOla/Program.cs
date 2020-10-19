fe4feca25e97a395063774bec6ebb19daefe9f53
barbaradeveloper <barbaramartins1245@gmail.com>
2020-10-19 15:21:01 -0300

    Exercício 1 Concluído

---
+++ GitOla/GitOla/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitOla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista de Exercícios");
+        }
+    }
+}
