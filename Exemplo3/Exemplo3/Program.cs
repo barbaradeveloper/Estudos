93e6372dda28dcd8431a409b4a683bf1fc2752e4
barbaradeveloper <barbaramartins1245@gmail.com>
2020-10-19 15:40:12 -0300

    Exercício 3

---
+++ Exemplo3/Exemplo3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendendo git e github");
+        }
+    }
+}
